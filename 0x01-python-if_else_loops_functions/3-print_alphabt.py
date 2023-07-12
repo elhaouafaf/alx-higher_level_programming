7034cc0dff86372243a4095bb307f8f7e308815e
elhaouafaf <elhaou.afaf93@gmail.com>
2023-07-12 16:33:28 -0700

    0 till 12

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-# 4-print_hexa.py
-
-for number in range(0, 99):
-    print("{} = {}".format(number, hex(number)))
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
