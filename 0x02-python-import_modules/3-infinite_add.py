9a5d0063c80966100b35757f3bca9ebaa4b87f4d
elhaouafaf <elhaou.afaf93@gmail.com>
2023-07-18 15:52:06 +0200

    hgfds

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
-
+from sys import argv
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
-    import sys
-
-
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    res = 0
+    for ar in range(1, len(argv)):
+        res = res + int(argv[ar])
+    print("{}".format(res))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-
-
+import hidden_4
 if __name__ == "__main__":
-    """Print all names defined by hidden_4 module."""
-    import hidden_4
-
-
-    names = dir(hidden_4)
-    for name in names:
+    folder = dir(hidden_4)
+    for name in folder:
         if name[:2] != "__":
-            print(name)
-
+            print("{}".format(name))
